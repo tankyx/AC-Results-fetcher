3c4225f680613edcf224a9f4ae107881d2b3901d
tankyx <tanguy.pedrazzoli@epitech.eu>
2018-12-03 20:08:43 +0100

    Created new component
    component result

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'beautify-scrollbar/dist/index.css';
+import 'v2-table/dist/index.css';
+import V2Table from 'v2-table';
 
 Vue.config.productionTip = false;
+Vue.use(V2Table)
+
+var track = new Vue({
+  data: {
+    name: ''
+  },
+  template: '<div>{{ message }}</div>'
+})
+
+track.name = "Rainbow Road"
 
 new Vue({
   router,
--- src/views/Results.vue
+++ src/views/Results.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="results">
     <h1>This is the results page</h1>
+    <ResultsTable></ResultsTable>
   </div>
 </template>
+
+<script>
+import ResultsTable from '@/components/ResultsTable.vue'
+
+export default {
+  name: 'Results',
+  components: {
+    ResultsTable
+  }
+}
+</script>
