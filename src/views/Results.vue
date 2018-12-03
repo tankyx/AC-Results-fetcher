555b1c6393e5bfa75fc005688e1ed120ebf639aa
tankyx <tanguy.pedrazzoli@epitech.eu>
2018-12-03 18:40:13 +0100

    delete grid
    delete useless grid

--- src/views/Results.vue
+++ src/views/Results.vue
@@ -2,34 +2,4 @@
   <div class="results">
     <h1>This is the results page</h1>
   </div>
-  <div id="demo">
-    <demo-grid
-      :data="gridData"
-      :columns="gridColumns"
-      :filter-key="searchQuery">
-    </demo-grid>
-  </div>
 </template>
-
-<script type="text/x-template" id="grid-template">
-  <table>
-    <thead>
-      <tr>
-        <th v-for="key in columns"
-          v-on:click="sortBy(key)"
-          v-bind:class="{ active: sortKey == key }">
-          {{ key , capitalize }}
-          <span class="arrow" v-bind:class="sortOrders[key] > 0 ? 'asc' : 'dsc'">
-          </span>
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="entry in filteredData">
-        <td v-for="key in columns">
-          {{entry[key]}}
-        </td>
-      </tr>
-    </tbody>
-  </table>
-</script>
